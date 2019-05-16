5856b2e0a40abccf49395e220bbeb7b7121a9d88
Joe Ciesielski <joseph.ciesielski@philamuseum.org>
2019-05-16 13:11:55 -0400

    adding win prob

--- tmc-dashboard/app.R
+++ tmc-dashboard/app.R
@@ -51,19 +51,30 @@ ui <- fluidPage(
                            district_choices,
                            selected = "all"
             ),
-            dateRangeInput("daterange",
-                           "Date Range",
-                           start = "2018-08-01",
-                           end = "2018-11-06")
+            dateInput("daterange",
+                      "Prediction Date",
+                      value = "2018-11-06",
+                      min = "2018-08-01",
+                      max = "2018-11-06"
+            ),
+            sliderInput("closecount",
+                        "Number of close races to show",
+                        min = 1,
+                        max = 435, 
+                        value = 5
+            )
         ),
         
         mainPanel(
             h3(textOutput("date_max")),
             br(),
+            h4("Predicted wins by party"),
             tableOutput("party_win"),
             br(),
+            h4("Change in predicted voteshare over time"),
             plotOutput("voteshare_plot"),
             br(),
+            h4("Closest Races (by probability of winning)"),
             tableOutput("closest_races")
         )
     )
@@ -89,10 +100,7 @@ server <- function(input, output, session) {
          } 
         
        data %>% 
-           filter(
-               forecastdate >= input$daterange[[1]],
-               forecastdate <= input$daterange[[2]]
-           ) %>% 
+           filter(forecastdate <= input$daterange) %>% 
            group_by(party, forecastdate) %>% 
            summarise_at(vars(contains("voteshare"), win_probability), mean)
     })
@@ -116,33 +124,35 @@ server <- function(input, output, session) {
         } 
         
         data %>% 
-            filter(
-                forecastdate >= input$daterange[[1]],
-                forecastdate <= input$daterange[[2]]
-            ) %>% 
+            filter(forecastdate <= input$daterange) %>% 
             filter(forecastdate == max(forecastdate)) %>% 
-            select(district, party, win_probability) %>% 
+            select(district, party, win_probability,
+                   starts_with("voteshare")) %>% 
             arrange(district)
         
     })
     
     output$party_win <- renderTable({
         
-        filtered_candidates() %>% 
+        dat <- filtered_candidates() %>% 
             group_by(district) %>% 
             mutate(
-                win = if_else(win_probability == max(win_probability), 1, 0)
+                win = if_else(voteshare > 0.5, 1, 0),
+                tossup = if_else(max(voteshare) <= 0.5, 1L, 0L)
             ) %>% 
-            ungroup() %>%
-            complete(district, party, fill = list(win = 0)) %>%
+            ungroup()
+             
+         party <- dat %>% 
             group_by(party) %>% 
-            summarise(
-                percent_winning = mean(win),
-                total_winning = sum(win)
-            )
-        
+            summarise(win = as.integer(sum(win))) %>% 
+            spread(party, win)
+         
+         tossup <- dat %>% 
+             distinct(district, tossup) %>% 
+             summarise(`toss-up` = sum(tossup))
+         
+         bind_cols(party, tossup)
     })
-    
     
     output$voteshare_plot <- renderPlot({
         
@@ -160,7 +170,6 @@ server <- function(input, output, session) {
                 col_scale + 
                 theme_minimal() +
                 labs(
-                    title = "Predicted voteshare by party",
                     x = "date of forecast",
                     y = "average predicted voteshare"
                 )
@@ -182,16 +191,16 @@ server <- function(input, output, session) {
     output$closest_races <- renderTable({
         
         # need to correct this
-        # filtered_candidates() %>% 
-        #     voteshare_tidy %>% 
-        #     filter(forecastdate == date) %>% 
-        #     select(district, party, win_probability) %>% 
-        #     spread(party, win_probability) %>% 
-        #     mutate(dif = abs(Democrat - 0.5)) %>% 
-        #     arrange(desc(dif)) %>% 
-        #     top_n(5) %>% 
-        #     select(-dif)
-        
+        filtered_candidates() %>%
+            select(district, party, win_probability) %>%
+            spread(party, win_probability) %>%
+            mutate(dif = abs(Democrat - Republican)) %>%
+            top_n(input$closecount, wt = -dif) %>%
+            select(-dif) %>% 
+            mutate_if(is.numeric, function(x)
+                if_else(is.na(x), NA_character_, percent(x, accuracy = .01))
+                )
+
         
     })
 
